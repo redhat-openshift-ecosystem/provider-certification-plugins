c0490a51d48fe4add1b16d9218bccfbdd72b94ec
Marco Braga <mrbraga@redhat.com>
2022-03-25 17:28:12 -0300

    feat(tests-plugin): moving tests to tool dir

--- tools/openshift-partner-cert/hack/build-openshift-tests-image.sh
+++ tools/openshift-partner-cert/hack/build-openshift-tests-image.sh
@@ -3,13 +3,14 @@
 # Build openshift-tests binary
 # https://github.com/openshift/origin#end-to-end-e2e-and-extended-tests
 
+PULL_SECRET="${HOME}/.openshift/pull-secret-latest.json"
 tmp_origin="./tmp/origin"
 #rm -rf tmp/origin
 git clone git@github.com:openshift/origin.git $tmp_origin
 
 pushd ${tmp_origin}
 podman build \
-    --authfile $(PULL_SECRET) \
+    --authfile ${PULL_SECRET} \
     -t openshift-tests:latest \
     -f images/tests/Dockerfile.rhel .
 popd
