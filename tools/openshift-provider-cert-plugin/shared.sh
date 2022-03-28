b65ee8418d385cc5ec1bb2375db4358d6a94c8f1
Marco Braga <mrbraga@redhat.com>
2022-03-28 09:42:39 -0300

    fix(plugin-cert): exec runtime and feedback

--- tools/openshift-provider-cert-plugin/shared.sh
+++ tools/openshift-provider-cert-plugin/shared.sh
@@ -1,5 +1,17 @@
-results_dir="${RESULTS_DIR:-/tmp/sonobuoy/results}"
-results_pipe="${results_dir}/status_pipe"
+#!/usr/bin/env sh
 
-results_script_dir="${results_dir}/plugin-scripts"
+os_log_info() {
+    echo "#$(caller |awk '{print$2":"$1}')> " $@
+}
+export -f os_log_info
+
+sys_trap_error(){
+    os_log_info "ERROR on line $(caller)" >&2
+}
+trap sys_trap_error ERR
+
+export results_dir="${RESULTS_DIR:-/tmp/sonobuoy/results}"
+export results_pipe="${results_dir}/status_pipe"
+
+export results_script_dir="${results_dir}/plugin-scripts"
 test -d ${results_script_dir} || mkdir -p ${results_script_dir}
