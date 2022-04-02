ae26ceeb0c8e9e016098c4295e3171789643526a
Marco Braga <mrbraga@redhat.com>
2022-04-02 02:52:56 -0300

    refact(plugin-cert): report progress checks dep plugin w/ msg field #2

--- tools/openshift-provider-cert-plugin/executor.sh
+++ tools/openshift-provider-cert-plugin/executor.sh
@@ -11,15 +11,15 @@ set -o nounset
 
 os_log_info "[executor] Starting..."
 
-export KUBECONFIG=/tmp/kubeconfig
+#export KUBECONFIG=/tmp/kubeconfig
 
 suite="${E2E_SUITE:-kubernetes/conformance}"
-ca_path="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
-sa_token="/var/run/secrets/kubernetes.io/serviceaccount/token"
+#ca_path="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+#sa_token="/var/run/secrets/kubernetes.io/serviceaccount/token"
 
-os_log_info "[executor] Checking credentials are present..."
-test ! -f "${ca_path}" || os_log_info "[executor] file not found=${ca_path}"
-test ! -f "${sa_token}" || os_log_info "[executor] file not found=${sa_token}"
+os_log_info "[executor] Checking if credentials are present..."
+test ! -f "${SA_CA_PATH}" || os_log_info "[executor] secret not found=${SA_CA_PATH}"
+test ! -f "${SA_TOKEN_PATH}" || os_log_info "[executor] secret not found=${SA_TOKEN_PATH}"
 
 #
 # openshift login
@@ -27,8 +27,8 @@ test ! -f "${sa_token}" || os_log_info "[executor] file not found=${sa_token}"
 
 os_log_info "[executor] Login to OpenShift cluster locally..."
 oc login https://172.30.0.1:443 \
-    --token=$(cat ${sa_token}) \
-    --certificate-authority=${ca_path} || true;
+    --token="$(cat ${SA_TOKEN_PATH})" \
+    --certificate-authority="${SA_CA_PATH}" || true;
 
 #
 # Executor options
@@ -41,13 +41,13 @@ if [[ ! -z ${CERT_TEST_FILE:-} ]]; then
     os_log_info "Running openshift-tests for custom tests [${CERT_TEST_FILE}]..."
     if [[ -s ${CERT_TEST_FILE} ]]; then
         openshift-tests run \
-            --junit-dir ${results_dir} \
+            --junit-dir ${RESULTS_DIR} \
             -f ${CERT_TEST_FILE} \
-            | tee ${results_pipe} || true
+            | tee -a "${RESULTS_PIPE}" || true
         os_log_info "openshift-tests finished"
     else
         os_log_info "the file provided has no tests. Sending progress and finish executor...";
-        echo "(0/0/0)" > ${results_pipe}
+        echo "(0/0/0)" > ${RESULTS_PIPE}
     fi
 
 # reusing script to parser jobs.
@@ -63,9 +63,9 @@ elif [[ ! -z ${CUSTOM_TEST_FILTER_SIG:-} ]]; then
 
     os_log_info "#executor>Running"
     openshift-tests run \
-        --junit-dir ${results_dir} \
+        --junit-dir ${RESULTS_DIR} \
         -f ./tmp/openshift-e2e-suites.txt \
-        | tee ${results_pipe}
+        | tee -a "${RESULTS_PIPE}" || true
 
 # Filter by string pattern from 'all' tests
 elif [[ ! -z ${CUSTOM_TEST_FILTER_STR:-} ]]; then
@@ -73,15 +73,15 @@ elif [[ ! -z ${CUSTOM_TEST_FILTER_STR:-} ]]; then
     openshift-tests run --dry-run all \
         | grep "${CUSTOM_TEST_FILTER_STR}" \
         | openshift-tests run -f - \
-        | tee ${results_pipe}
+        | tee -a "${RESULTS_PIPE}" || true
 
 # Default execution - running default suite
 else
     os_log_info "#executor>Running default execution for openshift-tests suite [${suite}]..."
     openshift-tests run \
-        --junit-dir ${results_dir} \
+        --junit-dir ${RESULTS_DIR} \
         ${suite} \
-        | tee ${results_pipe}
+        | tee -a "${RESULTS_PIPE}" || true
 fi
 
 os_log_info "Plugin executor finished. Result[$?]";
