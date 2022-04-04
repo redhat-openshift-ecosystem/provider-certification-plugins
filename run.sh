7711a6d711c52547d42caea857860f8ed1b18efd
Marco Braga <mrbraga@redhat.com>
2022-04-04 18:22:35 -0300

    feat(cli): add custom status to view serial exec deps

--- run.sh
+++ run.sh
@@ -31,6 +31,7 @@ sonobuoy run \
     --dns-namespace openshift-dns \
     --dns-pod-labels=dns.operator.openshift.io/daemonset-dns=default \
     --timeout ${PLUGIN_TIMEOUT} \
+    --plugin tools/plugins/level-0-kube-conformance.yaml \
     --plugin tools/plugins/openshift-provider-cert-level-1.yaml \
     --plugin tools/plugins/openshift-provider-cert-level-2.yaml \
     --plugin tools/plugins/openshift-provider-cert-level-3.yaml
@@ -49,6 +50,37 @@ while true; do
         echo -e "\n\n$(date)> Sonobuoy is still running..."
         sonobuoy status
     fi
+    # Detailed status
+    st_file="/tmp/sonobuoy-status.json"
+    sonobuoy status --json > "${st_file}"
+    printf "\n\nGlobal Status: %s" "$(jq -r '.status // "Unknown"' ${st_file})"
+    printf "\n%-30s | %-10s | %-10s | %-25s | %-50s" \
+            "JOB_NAME" "STATUS" "RESULTS" "PROGRESS" "MESSAGE"
+    for plugin_name in $(jq -r '.plugins[].plugin' /tmp/sonobuoy-status.json |sort); do
+        pl_status=$(jq -r ".plugins[] | select (.plugin==\"${plugin_name}\").status" "${st_file}")
+        pl_result=$(jq -r ".plugins[] | select (.plugin==\"${plugin_name}\")[\"result-status\"]" "${st_file}" | tr -d '\n')
+
+        pl_prog_total=$(jq -r ".plugins[] | select (.plugin==\"${plugin_name}\").progress.total // \"\"" "${st_file}" | tr -d '\n')
+        pl_prog_comp=$(jq -r ".plugins[] | select (.plugin==\"${plugin_name}\").progress.completed // \"\"" "${st_file}" | tr -d '\n')
+        pl_prog_failed=$(jq -r ".plugins[] | select (.plugin==\"${plugin_name}\").progress.failures|length" "${st_file}" | tr -d '\n')
+        pl_progress="${pl_prog_comp}/${pl_prog_total} (${pl_prog_failed} failures)"
+
+        pl_count_fail=$(jq -r ".plugins[] | select (.plugin==\"${plugin_name}\")[\"result-counts\"].failed" "${st_file}" | tr -d '\n')
+        pl_count_pass=$(jq -r ".plugins[] | select (.plugin==\"${plugin_name}\")[\"result-counts\"].passed" "${st_file}" | tr -d '\n')
+
+        if [[ "${pl_status}" == "running" ]]; then
+            pl_msg=$(jq -r ".plugins[] | select (.plugin==\"${plugin_name}\") |.progress.msg // \"N/A\"" "${st_file}" | tr -d '\n')
+        elif [[ "${pl_result}" == "" ]]; then
+            pl_msg="waiting post-processor..."
+        else
+            pl_msg="Total tests processed: $(echo "$pl_count_pass + $pl_count_fail "|bc) (${pl_count_pass} pass / ${pl_count_fail} failed)"
+        fi
+
+        printf "\n%-30s | %-10s | %-10s | %-25s | %-50s" \
+                "${plugin_name}" "${pl_status}" "${pl_result}" \
+                "${pl_progress}" "${pl_msg}"
+    done
+
     sleep 10
 done
 
@@ -73,17 +105,31 @@ done
 echo -e "\nCollecting results..."
 sleep 10
 
-result_file=$(sonobuoy retrieve)
-RC=$?
-# TODO[1](release): need to collect artifacts if
-#  'sonobuoy retrieve' returned 'EOF' (download error).
-# https://github.com/mtulio/openshift-provider-certification/issues/4
-# TODO[2](asap): The filename could be set for 'retrieve' option,
-# so it can be an work arround while [1] is not fixed.
-if [[ ${RC} -ne 0 ]]; then
-    echo "One or more errors found to retrieve results"
-    exit ${RC}
-fi
+set +o errexit
+download_failed=true
+retries=0
+while ${download_failed}; do
+    result_file=$(sonobuoy retrieve)
+    RC=$?
+    # TODO[1](release): need to collect artifacts if
+    #  'sonobuoy retrieve' returned 'EOF' (download error).
+    # https://github.com/mtulio/openshift-provider-certification/issues/4
+    # TODO[2](asap): The filename could be set for 'retrieve' option,
+    # so it can be an work arround while [1] is not fixed.
+    if [[ ${RC} -eq 0 ]]; then
+        #echo "One or more errors found to retrieve results"
+        #exit ${RC}
+        download_failed=false
+    fi
+    retries=$(( retries + 1 ))
+    if [[ $retries -eq 10 ]]; then
+        echo "Retries timed out. Check 'sonobuoy retrieve' command."
+        exit 1
+    fi
+    echo "Error retrieving results. Waiting 10s to retry...[${retries}/10]"
+    sleep 10
+done
+set -o errexit
 
 if [[ -f ${result_file} ]]; then
     echo "Results saved at file ${result_file}"
