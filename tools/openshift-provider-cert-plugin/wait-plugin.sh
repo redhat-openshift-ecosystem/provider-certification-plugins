f1f0f35bc52427f2bb0e6a0edc3547d762e0853a
Marco Braga <mrbraga@redhat.com>
2022-04-04 18:23:53 -0300

    feat(plugin): add kube-conformance to stack

--- tools/openshift-provider-cert-plugin/wait-plugin.sh
+++ tools/openshift-provider-cert-plugin/wait-plugin.sh
@@ -5,45 +5,27 @@ set -o pipefail
 set -o nounset
 # set -o errexit
 
-os_log_info_waiter() {
+os_log_info_local() {
     os_log_info "$(date +%Y%m%d-%H%M%S)> [waiter] $*"
 }
 
-total_wait_timeout=1h
 os_log_info_waiter "Starting Level[${CERT_LEVEL:-}]..."
+set_config
 
-os_log_info_waiter "PLUGIN_BLOCKED_BY=${PLUGIN_BLOCKED_BY[*]}"
-if [[ "${CERT_LEVEL:-}" == "1" ]]
-then
-    os_log_info_waiter "Setting config for CERT_LEVEL=[${CERT_LEVEL:-}]"
-    #PLUGIN_BLOCKED_BY=()
-
-elif [[ "${CERT_LEVEL:-}" == "2" ]]
-then
-    os_log_info_waiter "Setting config for CERT_LEVEL=[${CERT_LEVEL:-}]" 
-    PLUGIN_BLOCKED_BY+=("openshift-provider-cert-level1")
-
-elif [[ "${CERT_LEVEL:-}" == "3" ]]
-then
-    os_log_info_waiter "Setting config for CERT_LEVEL=[${CERT_LEVEL:-}]"
-    PLUGIN_BLOCKED_BY+=("openshift-provider-cert-level2")
-fi
-os_log_info_waiter "PLUGIN_BLOCKED_BY=${PLUGIN_BLOCKED_BY[*]}"
-
-if [[ "${CERT_LEVEL:-}" != "1" ]]; then 
-    # Wait the pod to be running
-    os_log_info_waiter "Checking if Level[${CERT_LEVEL:-}] is blocked by labels [${PLUGIN_BLOCKED_BY[*]}] are ready..."
+os_log_info_local "Checking if there's plugins blocking Level${CERT_LEVEL:-} execution..."
+if [[ ${#PLUGIN_BLOCKED_BY[@]} -ge 1 ]]; then
+    os_log_info_local "Level${CERT_LEVEL:-} plugin is blocked by: [${PLUGIN_BLOCKED_BY[*]}]"
     for pod_label in "${PLUGIN_BLOCKED_BY[@]}"; do
         os_log_info_waiter "Waiting running pod-label: ${pod_label}"
         kubectl wait \
-            --timeout=${total_wait_timeout} \
+            --timeout=10m \
             --for=condition=ready pod \
             -l sonobuoy-plugin="${pod_label}"
     done
 
-    os_log_info_waiter "Resources ready! Waiting for Level[${CERT_LEVEL:-}]'s plugins is completed..."
+    os_log_info_local "Resources ready! Waiting for Level[${CERT_LEVEL:-}]'s plugins is completed..."
     for plugin_name in "${PLUGIN_BLOCKED_BY[@]}"; do
-        os_log_info_waiter "Waiting 'completed' condition for pod: ${plugin_name}"
+        os_log_info_local "Waiting 'completed' condition for pod: ${plugin_name}"
         
         # Wait until sonobuoy job is completed
         timeout_checks=0
@@ -52,22 +34,22 @@ if [[ "${CERT_LEVEL:-}" != "1" ]]; then
         do
             plugin_status=$(./sonobuoy status --json \
                 | jq -r ".plugins[] | select (.plugin == \"${plugin_name}\" ) |.status // \"\"")
-            os_log_info_waiter "Plugin[${plugin_name}] with status[${plugin_status}]..."
-            os_log_info_waiter "$(./sonobuoy status --json)"
+            os_log_info_local "Plugin[${plugin_name}] with status[${plugin_status}]..."
+            os_log_info_local "$(./sonobuoy status --json)"
             if [[ "${plugin_status}" == "complete" ]]; then
-                os_log_info_waiter "Plugin[${plugin_name}] with status[${plugin_status}] is completed!"
+                os_log_info_local "Plugin[${plugin_name}] with status[${plugin_status}] is completed!"
                 break
             fi
             timeout_checks=$(( timeout_checks + 1 ))
             if [[ ${timeout_checks} -eq ${timeout_count} ]]; then
-                os_log_info_waiter "Timeout waiting condition 'complete' for plugin[${plugin_name}]."
+                os_log_info_local "Timeout waiting condition 'complete' for plugin[${plugin_name}]."
                 exit 1
             fi
-            os_log_info_waiter "Waiting 30s for Plugin[${plugin_name}]...[${timeout_checks}/${timeout_count}]"
+            os_log_info_local "Waiting 30s for Plugin[${plugin_name}]...[${timeout_checks}/${timeout_count}]"
             sleep 30
         done
     done
-    os_log_info_waiter "All the conditions has been met!"
+    os_log_info_local "All the conditions has been met!"
 fi
 
-os_log_info_waiter "Done for Level[${CERT_LEVEL:-}]."
+os_log_info_local "Done for Level[${CERT_LEVEL:-}]."
