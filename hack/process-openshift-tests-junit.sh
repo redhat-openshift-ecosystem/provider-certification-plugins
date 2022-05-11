b09562a2a8e40246bee8a655dfc1ee26453bf9a1
Marco Braga <mrbraga@redhat.com>
2022-05-11 10:15:17 -0300

    chore/junit: add more insights extractors from junit

--- hack/process-openshift-tests-junit.sh
+++ hack/process-openshift-tests-junit.sh
@@ -1,7 +1,14 @@
 #!/bin/env bash
 
-for td in $(ls -d results/*/); do
-    echo "Processing ${td}"
+base_res_dir="./results"
+processed_dir="${base_res_dir}/processed"
+
+mkdir -p ${processed_dir}
+
+truncate -s 0 ${processed_dir}/filter-tag.txt
+
+for td in $(ls -d ${base_res_dir}/*/); do
+    echo -e "\n#> Processing ${td}"
     test_id=$(basename ${td})
     junit_file=$(ls ${td}/*.xml 2>/dev/null || true)
     if [[ ! -f ${junit_file} ]]; then
@@ -9,6 +16,12 @@ for td in $(ls -d results/*/); do
         continue
     fi
     xq -r ".testsuite | [\"${test_id}\", .[\"@time\"], .[\"@tests\"], .[\"@skipped\"], .[\"@failures\"]] | @tsv" "${junit_file}"
+
+    # extract all test list
+    all_file="${processed_dir}/${test_id}-all.txt"
+    echo "#>> Writing all file to ${all_file}"
+    xq  '.testsuite.testcase[]["@name"]' "${junit_file}" | sort > "${all_file}"
+
+    echo "#>> Extracting first tag and saving to ${processed_dir}/filter-tag.txt"
+    awk -F'\]' '{print$1}' "${all_file}" |awk -F'[' '{print$2}' |sort |uniq -c |awk -v test_id="${test_id}" '{print test_id ";" $2 ";" $1 }' |sort >> ${processed_dir}/filter-tag.txt
 done
-
-
