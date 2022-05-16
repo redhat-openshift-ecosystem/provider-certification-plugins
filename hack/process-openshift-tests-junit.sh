b1115b54edd1afa15744383dc371e490fa94bd97
Robert Bost <rbost@redhat.com>
2022-05-16 10:01:38 -0600

    fix(hack): make shellcheck happy

--- hack/process-openshift-tests-junit.sh
+++ hack/process-openshift-tests-junit.sh
@@ -7,12 +7,12 @@ mkdir -p ${processed_dir}
 
 truncate -s 0 ${processed_dir}/filter-tag.txt
 
-for td in $(ls -d ${base_res_dir}/*/); do
-    echo -e "\n#> Processing ${td}"
-    test_id=$(basename ${td})
-    junit_file=$(ls ${td}/*.xml 2>/dev/null || true)
+for td in "${base_res_dir}"/*/; do
+    echo -e "\n#> Processing" "${td}"
+    test_id=$(basename "${td}")
+    junit_file=$(ls "${td}"/*.xml 2>/dev/null || true)
     if [[ ! -f ${junit_file} ]]; then
-        echo "There's no junit files inside ${test_id}"
+        echo "There's no junit files inside" "${test_id}"
         continue
     fi
     xq -r ".testsuite | [\"${test_id}\", .[\"@time\"], .[\"@tests\"], .[\"@skipped\"], .[\"@failures\"]] | @tsv" "${junit_file}"
