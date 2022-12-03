312fefc3da35b34a080f3d3ac9a5569db0499102
Marco Braga <mrbraga@redhat.com>
2022-12-03 08:28:38 -0800

    fix: typos on plugins information, id and suite

--- openshift-tests-provider-cert/plugin/global_env.sh
+++ openshift-tests-provider-cert/plugin/global_env.sh
@@ -45,7 +45,7 @@ declare -grx PLUGIN_ID_OPENSHIFT_ARTIFACTS_COLLECTOR="99"
 # PLUGIN Instances
 declare -grx PLUGIN_ID_KUBE_CONFORMANCE="10"
 declare -grx PLUGIN_NAME_KUBE_CONFORMANCE="${PLUGIN_ID_KUBE_CONFORMANCE}-openshift-kube-conformance"
-declare -grx OPENSHIFT_TESTS_SUITE_KUBE_CONFORMANCE="kubernentes/conformance"
+declare -grx OPENSHIFT_TESTS_SUITE_KUBE_CONFORMANCE="kubernetes/conformance"
 
 declare -grx PLUGIN_ID_OPENSHIFT_CONFORMANCE="20"
 declare -grx PLUGIN_NAME_OPENSHIFT_CONFORMANCE="${PLUGIN_ID_OPENSHIFT_CONFORMANCE}-openshift-conformance-validated"
